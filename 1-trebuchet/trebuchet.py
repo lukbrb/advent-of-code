edd1bc425371b41ce7b16ced7b21a55cc2eabdf9
lukbrb <81429406+lukbrb@users.noreply.github.com>
2023-12-05 23:45:47 +0100

    aoc jour 1

---
+++ 1-trebuchet/trebuchet.py
@@ -0,0 +1,63 @@
+with open('input') as f:
+    lines = f.readlines()
+
+
+def is_digit(item):
+    try:
+        int(item)  # pas besoin de le changer en int, pour pouvoir concatener à la fin
+        return True
+    except Exception as e:
+        return False
+
+def extract_line_digit(ligne):
+    digits = []
+    for item in ligne:
+        if is_digit(item):
+            digits.append(item)
+    if digits:  # si liste non vide
+        value = digits[0] + digits[-1]
+    else: value = 0
+    return int(value)
+
+
+def solve_part1():
+    somme = 0
+    for line in lines:
+        somme += extract_line_digit(line)
+    print(somme)
+
+digitnames = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+def replace_name_by_digits(ligne):
+    print(ligne)
+    digits = []
+    nums_found = []
+    for num in digitnames.keys():
+        start_idx = ligne.find(num)
+        if start_idx == -1:
+            continue
+        end_idx = start_idx + len(num)
+        digits.append(start_idx)
+        digits.append(end_idx)
+        nums_found.append(num)
+    print(digits)
+    print(nums_found)
+    if len(digits) != len(set(digits)):
+        overlap = True
+        print('Overlap')
+
+        
+
+for line in lines:
+    replace_name_by_digits(line)
+
